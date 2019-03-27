8a23d3fa609e184c41b842095470b8e111a6123d
hocineimail <hocineismail75@gmail.com>
2019-03-27 04:16:38 +0100

    Display the Validation errors

--- frontend/client/src/components/Register.js
+++ frontend/client/src/components/Register.js
@@ -1,10 +1,10 @@
-// Register.js
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { registerUser } from '../actions/authentication';
+import classnames from 'classnames';
 
 class Register extends Component {
 
@@ -47,6 +47,7 @@ class Register extends Component {
     }
 
     render() {
+        const { errors } = this.state;
         return(
         <div className="container" style={{ marginTop: '50px', width: '700px'}}>
             <h2 style={{marginBottom: '40px'}}>Registration</h2>
@@ -55,41 +56,53 @@ class Register extends Component {
                     <input
                     type="text"
                     placeholder="Name"
-                    className="form-control"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.name
+                    })}
                     name="name"
                     onChange={ this.handleInputChange }
                     value={ this.state.name }
                     />
+                    {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                 </div>
                 <div className="form-group">
                     <input
                     type="email"
                     placeholder="Email"
-                    className="form-control"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.email
+                    })}
                     name="email"
                     onChange={ this.handleInputChange }
                     value={ this.state.email }
                     />
+                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                 </div>
                 <div className="form-group">
                     <input
                     type="password"
                     placeholder="Password"
-                    className="form-control"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.password
+                    })}
                     name="password"
                     onChange={ this.handleInputChange }
                     value={ this.state.password }
                     />
+                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <div className="form-group">
                     <input
                     type="password"
                     placeholder="Confirm Password"
-                    className="form-control"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.password_confirm
+                    })}
                     name="password_confirm"
                     onChange={ this.handleInputChange }
                     value={ this.state.password_confirm }
                     />
+                    {errors.password_confirm && (<div className="invalid-feedback">{errors.password_confirm}</div>)}
                 </div>
                 <div className="form-group">
                     <button type="submit" className="btn btn-primary">
